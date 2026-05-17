f516d9c6cf99b5f3222155505d2c04084672973a
turja007-rgb <agradipmodokturja@gmail.com>
2026-05-17 17:23:56 +0600

    initial commit

---
+++ views/activity_feed.php
@@ -0,0 +1,15 @@
+<div class="activity-page">
+    <h2>Project Activity</h2>
+    <input type="hidden" id="project_id" value="1"> 
+    
+    <select id="member-filter">
+        <option value="">All Members</option>
+        <option value="1">User 1</option> 
+        <option value="2">User 2</option>
+    </select>
+
+    <div id="activity-list">
+        </div>
+</div>
+
+<script src="../assets/js/activity.js"></script>
---
+++ views/task_details.php
@@ -0,0 +1,13 @@
+<div class="task-details">
+    <h3>Task Comments</h3>
+    <div id="comment-thread">
+        </div>
+    
+    <form id="comment-form">
+        <input type="hidden" id="task_id" value="1"> 
+        <textarea id="comment_body" placeholder="Write a comment..." required></textarea>
+        <button type="submit">Post Comment</button>
+    </form>
+</div>
+
+<script src="../assets/js/comments.js"></script>
